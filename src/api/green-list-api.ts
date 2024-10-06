8888b83e43b68495bb111d643775a0cb7fea8ad3
Juan Ramos <juan2ramos@gmail.com>
2024-10-06 18:53:12 -0500

    chore: envs amplify

--- src/api/green-list-api.ts
+++ src/api/green-list-api.ts
@@ -5,7 +5,11 @@ const nodeEnv = import.meta.env.NODE_ENV;
 const apiProduction = import.meta.env.VITE_API_PRODUCTION_GREEN_LIST_URL;
 const apiStaging = import.meta.env.VITE_API_STAGING_GREEN_LIST_URL;
 
+console.log('import.meta.env', import.meta.env);
+console.log('nodeEnv', nodeEnv);
+
 const baseURL = nodeEnv === 'production' ? apiProduction : apiStaging;
+console.log('baseURL', baseURL);
 const greenListApi = axios.create({ baseURL });
 
 greenListApi.interceptors.request.use((config) => {
